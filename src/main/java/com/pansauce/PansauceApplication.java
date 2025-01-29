2253c552616f4c9427596c91b5ae1bbbc537310e
Tymko <kondrashko13@proton.me>
2025-01-29 20:17:08 +0200

    Added Spring Boot base

---
+++ src/main/java/com/pansauce/PansauceApplication.java
@@ -0,0 +1,13 @@
+package com.pansauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PansauceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PansauceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pansauce/PansauceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pansauce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PansauceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
