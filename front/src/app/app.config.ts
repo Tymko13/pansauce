8c9469feb3986109f02fd1d621512a89b0774f13
Tymko <kondrashko13@proton.me>
2025-04-24 14:57:35 +0300

    refactor: remove unused imports

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './_auth/auth.interceptor';
 import {provideNativeDateAdapter} from '@angular/material/core';
 
