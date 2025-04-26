7e7b7f5d15dc0ee0cd6691b600bd13a3bad6b508
Tymko <kondrashko13@proton.me>
2025-04-26 18:02:59 +0300

    fix: date queries dont work 3

--- src/main/java/com/pansauce/repository/BatchRepository.java
+++ src/main/java/com/pansauce/repository/BatchRepository.java
@@ -109,13 +109,15 @@ public class BatchRepository implements BatchDao {
 
     @Override
     public TotalIncome getIncomeFromSoldBatchesBetweenDates(Date from, Date to) {
-        List<TotalIncome> result = jdbc.query(GET_TOTAL_INCOME_FROM_SOLD_BATCHES_BETWEEN_DATES, TOTAL_INCOME_ROW_MAPPER);
+        List<TotalIncome> result = jdbc.query(GET_TOTAL_INCOME_FROM_SOLD_BATCHES_BETWEEN_DATES, TOTAL_INCOME_ROW_MAPPER,
+                                              from, to);
         return result.isEmpty() ? null : result.getFirst();
     }
 
     @Override
     public TotalAmount getAmountOfSoldBatchesBetweenDates(Date from, Date to) {
-        List<TotalAmount> result = jdbc.query(GET_TOTAL_AMOUNT_OF_SOLD_BATCHES_BETWEEN_DATES, TOTAL_AMOUNT_ROW_MAPPER);
+        List<TotalAmount> result = jdbc.query(GET_TOTAL_AMOUNT_OF_SOLD_BATCHES_BETWEEN_DATES, TOTAL_AMOUNT_ROW_MAPPER,
+                                              from, to);
         return result.isEmpty() ? null : result.getFirst();
     }
 
