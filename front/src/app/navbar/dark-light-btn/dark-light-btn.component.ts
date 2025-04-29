ad647adeb44c3892d84648f6bb96dcf48ba66145
Tymko <kondrashko13@proton.me>
2025-04-29 15:09:46 +0300

    refactor: clean light dark theme button

--- front/src/app/navbar/dark-light-btn/dark-light-btn.component.ts
+++ front/src/app/navbar/dark-light-btn/dark-light-btn.component.ts
@@ -44,7 +44,6 @@ export class DarkLightBtnComponent {
     document.documentElement.style.setProperty('--primary-color', '#24272B');
     document.documentElement.style.setProperty('--secondary-color', '#4A525A');
     document.documentElement.style.setProperty('--text-color', '#FFFFFF');
-    document.documentElement.style.setProperty('--text-color', '#FFFFFF');
     document.documentElement.style.setProperty('--white-color', '#6a5454')
 
   }
