a6abbace505cf170b3f96cb349c17b5709652577
Tymko <kondrashko13@proton.me>
2025-05-01 00:19:51 +0300

    style: table rows highlight on hover

--- front/src/app/info/sauce/sauce.component.ts
+++ front/src/app/info/sauce/sauce.component.ts
@@ -138,7 +138,6 @@ export class SauceComponent {
           typeNumber: res.typeNumber,
           typeName: res.typeName
         }
-        console.log(updatedSauce);
         this.sauceService.updateSauce(updatedSauce).subscribe(() => {
           this.updateDB();
         });
