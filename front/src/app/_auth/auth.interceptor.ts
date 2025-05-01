5bb3289d28c092fcafd26a696dc66001b84d7831
Tymko <kondrashko13@proton.me>
2025-05-01 16:24:19 +0300

    fix: error interceptor FINAL fix

--- front/src/app/_auth/auth.interceptor.ts
+++ front/src/app/_auth/auth.interceptor.ts
@@ -19,11 +19,11 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 }
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if(error.status == 401 || error.status == 403) {
         localStorage.removeItem('auth-token');
-        const router = inject(Router);
         router.navigate(["/login"]);
       }
       return throwError(() => error);
