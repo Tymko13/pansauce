a1a0633607b741b3fdbc751dee3b1c4f8882b7ed
Tymko <kondrashko13@proton.me>
2025-05-03 10:05:52 +0300

    feat: login error response

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,18 +1,19 @@
-import {Component, inject } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 import {AuthService} from '../_auth/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   standalone: true,
   templateUrl: 'login.component.html',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInput, MatButton],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInput, MatButton, NgIf],
   styleUrl: 'login.component.css'
 })
-export class LoginComponent{
+export class LoginComponent {
   private authService = inject(AuthService);
   private fb = inject(FormBuilder);
   private route = inject(ActivatedRoute);
@@ -22,6 +23,7 @@ export class LoginComponent{
     if (this.authService.isLoggedIn()) {
       this.router.navigate(['/']).catch(err => console.log(err));
     }
+    this.loginForm.valueChanges.subscribe(() => this.setError(false))
   }
 
   loginForm = this.fb.group({
@@ -29,11 +31,30 @@ export class LoginComponent{
     password: ['', Validators.required]
   });
 
+  error = false;
+
+  setError(error: boolean) {
+    this.error = error;
+    if(error) {
+      this.loginForm.get('username')?.setErrors({wrong: true});
+      this.loginForm.get('password')?.setErrors({wrong: true});
+    } else {
+      this.loginForm.get('username')?.setErrors(null);
+      this.loginForm.get('password')?.setErrors(null);
+    }
+  }
+
   submit() {
-    if(this.loginForm.valid) {
-      this.authService.login({username: this.loginForm.value.username!, password: this.loginForm.value.password!}).subscribe(() => {
-        this.router.navigate([this.route.snapshot.queryParams['returnUrl'] || '/'])
-          .catch(err => console.log(err));
+    if (this.loginForm.valid) {
+      this.authService.login({
+        username: this.loginForm.value.username!,
+        password: this.loginForm.value.password!
+      }).subscribe({
+        next: data => {
+          this.router.navigate([this.route.snapshot.queryParams['returnUrl'] || '/'])
+            .catch(err => console.log(err));
+        },
+        error: error => this.setError(true)
       });
     }
   }
